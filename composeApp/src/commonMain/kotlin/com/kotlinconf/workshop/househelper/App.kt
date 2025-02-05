36d754edca8806de692148d9d78fd6ae4aa2880a
Victor Kropp <victor.kropp@jetbrains.com>
2025-02-05 13:29:04 +0100

    add sample resources

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/App.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/App.kt
@@ -1,9 +1,20 @@
 package com.kotlinconf.workshop.househelper
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import househelper.composeapp.generated.resources.*
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    Column {
+        Image(painterResource(Res.drawable.lightbulb), contentDescription = stringResource(Res.string.lightbulb))
+        Image(painterResource(Res.drawable.switch), contentDescription = stringResource(Res.string.switch))
+        Image(painterResource(Res.drawable.humidity), contentDescription = stringResource(Res.string.humidity))
+        Image(painterResource(Res.drawable.thermostat), contentDescription = stringResource(Res.string.thermostat))
+    }
 }
