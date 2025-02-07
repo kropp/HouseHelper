4aac1f55a3c6b58256a8491178bb1b4c6aa42e52
Victor Kropp <victor.kropp@jetbrains.com>
2025-02-07 10:40:46 +0100

    setup hot reload

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
 }
 
 kotlin {
@@ -104,14 +105,18 @@ dependencies {
     debugImplementation(compose.uiTooling)
 }
 
+// Android preview support
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
+
 compose.desktop {
     application {
         mainClass = "com.kotlinconf.workshop.househelper.MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.kotlinconf.workshop.househelper"
-            packageVersion = "1.0.0"
-        }
     }
+}
+
+// Hot reload support
+composeCompiler {
+    featureFlags.add(ComposeFeatureFlag.OptimizeNonSkippingGroups)
 }
--- composeApp/src/desktopMain/kotlin/com/kotlinconf/workshop/househelper/main.kt
+++ composeApp/src/desktopMain/kotlin/com/kotlinconf/workshop/househelper/main.kt
@@ -2,12 +2,15 @@ package com.kotlinconf.workshop.househelper
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "HouseHelper",
-    ) {
-        App()
+    DevelopmentEntryPoint {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "HouseHelper",
+        ) {
+            App()
+        }
     }
 }
