e8c3ee7220576d758b0a35290a47514b36d7c259
Márton Braun <marton.braun@jetbrains.com>
2025-02-13 11:57:03 +0100

    update light colors

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/DeviceConstants.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/DeviceConstants.kt
@@ -9,17 +9,17 @@ object DeviceConstants {
         const val MAX_BRIGHTNESS = 100
 
         val PREDEFINED_COLORS = listOf(
-            Color.White,
-            Color(255, 255, 0),   // Yellow
-            Color(255, 165, 0),   // Orange
-            Color.Red,
-            Color.Green,
-            Color.Blue,
-            Color(128, 0, 128),   // Purple
-            Color(255, 192, 203)  // Pink
+            Color(255, 245, 235),  // Soft White
+            Color(255, 182, 203),  // Pastel Pink
+            Color(210, 170, 255),  // Pastel Lavender
+            Color(135, 206, 235),  // Pastel Blue
+            Color(144, 238, 144),  // Mint Green
+            Color(255, 250, 160),  // Soft Yellow
+            Color(255, 186, 128),  // Peach
+            Color(240, 128, 128),  // Light Coral
         )
 
-        val DEFAULT_COLOR = PREDEFINED_COLORS[0] // White
+        val DEFAULT_COLOR = PREDEFINED_COLORS[0]
     }
 
     object Humidity {
