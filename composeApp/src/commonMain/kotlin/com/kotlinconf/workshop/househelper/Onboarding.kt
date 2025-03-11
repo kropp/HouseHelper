a45af868dfee0a5944f80e278c8be0e8b9e0bcd1
Márton Braun <marton.braun@jetbrains.com>
2025-03-11 21:59:13 +0100

    Remove unused imports

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/Onboarding.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/Onboarding.kt
@@ -14,10 +14,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -35,12 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import househelper.composeapp.generated.resources.Res
-import househelper.composeapp.generated.resources.onboarding_next_button
-import househelper.composeapp.generated.resources.onboarding_welcome
-import househelper.composeapp.generated.resources.onboarding_about
-import org.jetbrains.compose.resources.StringResource
-import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun Onboarding(
