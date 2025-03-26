e149718a543de2eac3af3c2a3bd89e4c07835415
Márton Braun <marton.braun@jetbrains.com>
2025-03-26 21:50:56 +0100

    Task 8: Adding ViewModels to Koin

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/Koin.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/Koin.kt
@@ -1,8 +1,10 @@
 package com.kotlinconf.workshop.househelper
 
 import com.kotlinconf.workshop.househelper.dashboard.DashboardViewModel
+import com.kotlinconf.workshop.househelper.dashboard.RoomViewModel
 import com.kotlinconf.workshop.househelper.data.DemoHouseService
 import com.kotlinconf.workshop.househelper.data.HouseService
+import com.kotlinconf.workshop.househelper.devices.CameraDetailsViewModel
 import com.kotlinconf.workshop.househelper.devices.LightDetailsViewModel
 import com.kotlinconf.workshop.househelper.devices.RenameDeviceViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -16,10 +18,11 @@ fun createKoinConfig() = koinConfiguration {
     }
 
     val viewModelModule = module {
+        viewModelOf(::CameraDetailsViewModel)
         viewModelOf(::DashboardViewModel)
         viewModelOf(::LightDetailsViewModel)
         viewModelOf(::RenameDeviceViewModel)
-        // TODO Task 8: add more ViewModels
+        viewModelOf(::RoomViewModel)
     }
 
     modules(appModule, viewModelModule)
