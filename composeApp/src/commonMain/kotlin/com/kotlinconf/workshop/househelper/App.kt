a2657542f2342918f93dd090efa52cc3eaee0885
Márton Braun <marton.braun@jetbrains.com>
2025-04-20 20:02:55 +0200

    Task 10: Make the rename screen a dialog

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/App.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/App.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.kotlinconf.workshop.househelper.dashboard.DashboardScreen
@@ -117,7 +118,7 @@ fun App() {
                         },
                     )
                 }
-                composable<RenameDevice>(
+                dialog<RenameDevice>(
                     typeMap = mapOf(typeOf<DeviceId>() to DeviceIdNavType)
                 ) {
                     RenameDeviceScreen(
