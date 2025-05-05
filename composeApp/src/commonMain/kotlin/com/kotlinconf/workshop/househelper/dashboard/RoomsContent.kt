5d1c3c2a48146cb1cb9e4523f41195b14c29cec4
Victor Kropp <victor.kropp@jetbrains.com>
2025-05-05 12:09:40 +0200

    Task 22: Fix padding on Dashboard screen

--- composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/dashboard/RoomsContent.kt
+++ composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/dashboard/RoomsContent.kt
@@ -16,11 +16,16 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -83,6 +88,7 @@ fun RoomsContent(
 ) {
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
+        contentPadding = WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom).asPaddingValues(),
     ) {
         items(rooms) { room ->
             val roomViewModel: RoomViewModel = koinViewModel(key = room.id.value) {
